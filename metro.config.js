6234c5f2e771b959c30908e93600d2826d0b1836
roneyyb <bothra.rajat08@gmail.com>
2025-06-06 13:17:20 +0530

    ADDED REANIMATED AND NAVIGATION LIBRARY

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -8,4 +8,9 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  */
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
+const {
+    wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
+
+module.exports = wrapWithReanimatedMetroConfig(mergeConfig(getDefaultConfig(__dirname), config));

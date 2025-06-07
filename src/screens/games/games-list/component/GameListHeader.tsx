618fc32c7e97d4a492237a75250e104d16795680
roneyyb <bothra.rajat08@gmail.com>
2025-06-07 09:29:11 +0530

    UPDATED HEADER

--- src/screens/games/games-list/component/GameListHeader.tsx
+++ src/screens/games/games-list/component/GameListHeader.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Profile } from '@/assets/svg';
 import WrappedText from '@/components/text/WrappedText';
 import { useTheme } from '@/utils/theme';
+import { useUserDetails } from '@/utils/user';
 
 interface GameListHeaderProps {
     onPressProfile: Function
@@ -18,6 +19,7 @@ const GameListHeader = ({
 }: GameListHeaderProps) => {
 
     const { colors } = useTheme();
+    const { user } = useUserDetails()
 
     return (
         <View style={[styles.containerStyle]}>
@@ -33,6 +35,9 @@ const GameListHeader = ({
             </Pressable>
             <View style={{ marginLeft: 10, flex: 1 }}>
                 <WrappedText text="Games" textColor={colors.text} fontFamily="CircularStd-Bold" fontSize={24} />
+            </View>
+            <View style={{ boxShadow: '0 2px 4px #ffffff66', elevation: 10, backgroundColor: colors.cardBackground, paddingVertical: 5, borderRadius: 10, paddingHorizontal: 20 }}>
+                <WrappedText text={'Bal: ' + (user?.balance?.toString() || "0")} textColor={colors.text} fontFamily="CircularStd-Bold" fontSize={18} textStyle={{}} />
             </View>
         </View>
     )

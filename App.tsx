f5273a9d0270eb54cd16fb624ee31346072d1558
roneyyb <bothra.rajat08@gmail.com>
2025-06-17 20:50:19 +0530

    Minor fixes

--- App.tsx
+++ App.tsx
@@ -8,29 +8,26 @@
 import React from 'react';
 
 import StackNavigator from '@/navigation/StackNavigator';
-import { ThemeProviderSS } from './src/utils/theme';
-import { QueryClientProvider } from 'react-query';
+import {ThemeProviderSS} from './src/utils/theme';
+import {QueryClientProvider} from 'react-query';
 import queryClient from '@/utils/queryClient';
-import { UserProvider } from '@/utils/user';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import {UserProvider} from '@/utils/user';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import '@/utils/warnings'; // Import the warnings utility
 import '@/utils/axios';
 
 function App(): React.JSX.Element {
-
-    return (
-        <ThemeProviderSS>
-            <GestureHandlerRootView>
-                <QueryClientProvider client={queryClient}>
-                    <UserProvider>
-                        <StackNavigator />
-                    </UserProvider>
-                </QueryClientProvider>
-            </GestureHandlerRootView>
-        </ThemeProviderSS>
-
-    );
+  return (
+    <ThemeProviderSS>
+      <GestureHandlerRootView>
+        <QueryClientProvider client={queryClient}>
+          <UserProvider>
+            <StackNavigator />
+          </UserProvider>
+        </QueryClientProvider>
+      </GestureHandlerRootView>
+    </ThemeProviderSS>
+  );
 }
-
 
 export default App;
